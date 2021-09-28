1db6b8ab469cf9c24f4c04dcf2a5c2c6acc15257
TriiiHaus <nguytri8598@gmail.com>
2021-09-27 18:39:11 -0700

    google map marker

--- Main/controllers/api/map.js
+++ Main/controllers/api/map.js
@@ -1,10 +1,24 @@
 const mapkey = "AIzaSyCpSBx1lRYNTBGBAzxLUjWYaSLnKq1u1Us"
 
+// Map option
 function initMap(){
   const options = {
-    center: { lat: 33.7175, lng: =117.8311};
+    center: { lat: 33.7175, lng: -117.8311},
     zoom: 8
   }
 
+  //New Map
   map = new google.maps.Map(document.getElementById("map"), options)
+
+  //Marker
+  function addMarker(location){
+
+  
+    const marker = new google.maps.Marker({
+      position:location,
+      map: map 
+    });
+  }
+  addMarker{ lat: 33.7175, lng: -117.8311 }
+
 }
