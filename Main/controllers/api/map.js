d62c7d6ddb6dc8ce96e287a817993ba9c94341f7
lukesimon32 <lukesimon0@gmail.com>
2021-10-02 10:12:18 -0700

    committing to pull

--- Main/controllers/api/map.js
+++ Main/controllers/api/map.js
@@ -2,7 +2,7 @@ const mapkey = "AIzaSyCpSBx1lRYNTBGBAzxLUjWYaSLnKq1u1Us"
 
 function initMap(){
   const options = {
-    center: { lat: 33.7175, lng: =117.8311};
+    center: { lat: 33.7175, lng:  117.8311},
     zoom: 8
   }
 
