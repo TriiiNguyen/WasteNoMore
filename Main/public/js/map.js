31d598b87ec8d504140e7603af2451326a605373
TriiiHaus <nguytri8598@gmail.com>
2021-10-02 12:59:44 -0700

    worked on maps.hbs

--- Main/public/js/map.js
+++ Main/public/js/map.js
@@ -8,51 +8,30 @@ require('dotenv').config();
 
 
 
-const mapkey = "MAPKEY"
-
-generateMap([33.7175, -117.8311]);
+// const mapkey = "MAPKEY"
+// generateMap([33.7175, -117.8311])
 // front end function (will need API info)
 // JSon fetched info which is used in the below function
 
 
-function initMap(){
-  const options = {
-<<<<<<< HEAD
-    center: { lat: 33.7175, lng: -117.8311},
-=======
-    center: { lat: 33.7175, lng: - 117.8311 },
->>>>>>> a77ea3d883cbec5692db853ac85b07a727d3f006
-    zoom: 8
-  }
+function initMap() {
+  const options = { lat: 33.7175, lng: -117.8311 };
 
   //New Map
-  map = new google.maps.Map(document.getElementById("map"), options)
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: options,
+  });
 
-<<<<<<< HEAD
-  //Marker
-  function addMarker(location){
-
+  const marker = new google.maps.Marker({
+    position: options,
+    map: map,
+  });
+}
   
-    const marker = new google.maps.Marker({
-      position:location,
-      map: map 
-    });
-  }
-  addMarker{ lat: 33.7175, lng: -117.8311 }
-
-=======
-  function addMarker(location){
-    const marker = new google.maps.Marker({
-      position: location,
-      map:map,
-    
-    })
-  }
   addMarker({location{lat, lng}});
 
   //Listen for click on map location
   google.maps.event.addListener(map, "click", (event) => 
     addMarker({location:event.latLng})
   )
->>>>>>> a77ea3d883cbec5692db853ac85b07a727d3f006
-}
