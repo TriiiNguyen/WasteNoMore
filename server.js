4e26b1a18a9bb781aba657f9c76c8cbdc2e485c6
lukesimon32 <lukesimon0@gmail.com>
2021-10-03 21:28:51 -0700

    deleted conflicts

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const morgan = require('morgan-body');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 const handlebars = require('express-handlebars');
@@ -31,17 +32,11 @@ app.use(session(sess));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.get('/', function(req,res){
-  res.render('homepage')
-});
-
-app.get('/profile', function(req,res){
-  res.render('profile.handlebars')
-});
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+morgan(app);
 
 app.use(routes);
 
